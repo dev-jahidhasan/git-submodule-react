d652ac0fa35c3a3e9a1152250d79345363f11954
jahid-bd <jahidbd9x@gmail.com>
2024-08-09 22:49:59 +0600

    first implention of design library

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { useContext } from 'react';
+import { Alert, Button, ThemeContext, Typography, useTheme } from './lib/src';
+
 const App = () => {
-  return <div>App</div>;
+  const theme = useTheme();
+  const colorMode = useContext(ThemeContext);
+
+  return (
+    <div style={{ width: '80%', margin: '100px auto' }}>
+      <div>
+        <Typography variant="body2">Change theme:</Typography>
+        <div>
+          <Button onClick={colorMode.toggleColorMode} variant="contained">
+            Theme: {theme.palette.mode === 'dark' ? 'dark' : 'light'}
+          </Button>
+        </div>
+      </div>
+
+      <div>
+        <Alert sx={{ mt: 4 }}>This is success alert</Alert>
+        <Alert sx={{ mt: 4 }} severity="error">
+          This is Error alert
+        </Alert>
+      </div>
+    </div>
+  );
 };
 
 export default App;
